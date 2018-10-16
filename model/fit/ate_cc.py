99774aeb2bc3df5701168a3af786a8a764aa49fd
Jorge Rodriguez <jp.rodriguez.osorio@gmail.com>
2018-10-16 12:39:14 -0300

    Fit codes
    Included codes to evaluate fit of predictions from structural model

---
+++ model/fit/ate_cc.py
@@ -0,0 +1,84 @@
+#simulated: only until t=4 (t=5 almost no young)
+nperiods = 5
+cc_t = choices['choice_matrix']>=3
+cc_t = cc_t[:,0:nperiods,:]
+age_child = np.zeros((N,nperiods))
+for x in range(nperiods):
+	age_child[:,x]=agech0[:,0] + x
+
+#sample: young by t=2 (to be consistent with data)
+boo_t = passign[:,0]==1
+boo_c = passign[:,0]==0
+
+ate_cc = np.zeros((5))
+se_ate_cc = np.zeros((5))
+for t in range(5):
+	ate_cc[t] = np.mean(np.mean(cc_t[(boo_sample[:,0]) & (boo_t),t,:],axis = 0) - np.mean(cc_t[(boo_sample[:,0]) & (boo_c),t,:],axis = 0),axis=0)
+	se_ate_cc[t] = np.std(np.mean(cc_t[(boo_sample[:,0]) & (boo_t),t,:],axis = 0) - np.mean(cc_t[(boo_sample[:,0]) & (boo_c),t,:],axis = 0),axis=0)
+
+
+
+#data
+dofile = "/home/jrodriguez/understanding_NH/codes/model/fit/ate_cc.do"
+cmd = ["stata-se", "do", dofile]
+subprocess.call(cmd)
+ate_cc_obs=pd.read_csv('/home/jrodriguez/understanding_NH/results/Model/fit/ate_cc.csv').values
+se_ate_cc_obs=pd.read_csv('/home/jrodriguez/understanding_NH/results/Model/fit/se_ate_cc.csv').values
+
+ate_cc_obs_long=np.zeros((ate_cc.shape))
+se_ate_cc_obs_long=np.zeros((ate_cc.shape))
+i = 0
+for x in [1,4]:
+	ate_cc_obs_long[x] = ate_cc_obs[i,0].copy()
+	se_ate_cc_obs_long[x] = se_ate_cc_obs[i,0].copy()
+	i = i + 1
+
+#figure
+s1mask = ate_cc_obs_long!=0
+fig, ax=plt.subplots()
+x = np.array(range(0,nperiods))
+plot1=ax.plot(x[0:3],ate_cc[0:3]*100,'bs-',label='Simulated',alpha=0.6)
+plot4=ax.errorbar(x[0:3],ate_cc[0:3]*100,yerr=se_ate_cc[0:3]*100,ecolor='b',alpha=0.6)
+plot2=ax.plot(x[s1mask][0:1]+0.05,ate_cc_obs_long[s1mask][0:1]*100,'ko-',label='Data',alpha=0.9)
+plot3=ax.errorbar(x[s1mask][0:1]+0.05,ate_cc_obs_long[s1mask][0:1]*100,yerr=se_ate_cc_obs_long[s1mask][0:1]*100,fmt='ko',ecolor='k',alpha=0.9)
+plt.setp(plot1,linewidth=5)
+plt.setp(plot2,markeredgewidth=5)
+plt.setp(plot3,linewidth=3)
+plt.setp(plot4,linewidth=3)
+ax.legend(fontsize=20)
+ax.set_xticks([0, 1, 2])
+ax.set_xlim(-0.2,2.2)
+ax.set_ylim(0,35)
+ax.set_ylabel(r'Impact on child care (%)', fontsize=20)
+ax.set_xlabel(r'Years after random assignment ($t$)', fontsize=20)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
+plt.yticks(fontsize=15)
+plt.xticks(fontsize=15)
+plt.show()
+fig.savefig('/home/jrodriguez/understanding_NH/results/Model/fit/ate_cc.pdf', format='pdf')
+plt.close()
+
+
+nper = 1
+bar_width = 0.35
+fig, ax=plt.subplots()
+x = np.array(range(0,nper))
+plot1=ax.bar(x,ate_cc[1],bar_width,label='Simulated',color='k',alpha=0.8)
+plot2=ax.bar(x + bar_width,ate_cc_obs[0]*100,bar_width,yerr=se_ate_cc_obs[0,0],label='Data',edgecolor='k',
+	color='k',alpha=0.4)
+ax.legend(loc=8,fontsize=20)
+ax.set_ylabel('Impact on child care (%)', fontsize=20)
+ax.set_xlabel(r'$t=1$', fontsize=20)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
+ax.set_xticklabels([])
+ax.set_xticks([])
+plt.yticks(fontsize=15)
+plt.show()
+fig.savefig('/home/jrodriguez/understanding_NH/results/Model/fit/ate_bars_cc.pdf', format='pdf')
+plt.close()
---
+++ model/fit/ate_inc.do
@@ -0,0 +1,113 @@
+/*This do-file compute ATE on income using bootstrap
+*/
+
+
+clear
+program drop _all
+clear matrix
+clear mata
+set more off
+
+
+use "/home/jrodriguez/understanding_NH/results/Model/sample_model_v2.dta", clear
+set seed 2828
+local reps = 800
+
+program inc_diff, rclass
+	version 13
+	tempname mean_1 mean_2
+	args inc_year
+	qui: sum `inc_year' if d_RA==1 & agech_t2<=6
+	scalar `mean_1'=r(mean)
+	qui: sum `inc_year' if d_RA==0 & agech_t2<=6
+	scalar `mean_2'=r(mean)
+	return scalar ate=`mean_1' - `mean_2'
+end
+
+*Only 3 years
+mat ate_inc=J(3,1,.)
+mat se_ate_inc=J(3,1,.)
+
+*Income pc
+gen incomepc_t1_aux=(total_income_y1-cc_pay_t1*12)/(1 + nkids_year2 + married_year2)
+gen incomepc_t4_aux=(total_income_y4-cc_pay_t4*12)/(1 + nkids_year5 + married_year5)
+gen incomepc_t7_aux=(total_income_y7)/(1 + nkids_year8 + married_year8)
+
+replace incomepc_t1_aux=1 if incomepc_t1<0
+replace incomepc_t4_aux=1 if incomepc_t4<0
+replace incomepc_t7_aux=1 if incomepc_t7<0
+
+gen incomepc_t1=log(incomepc_t1_aux)
+gen incomepc_t4=log(incomepc_t4_aux)
+gen incomepc_t7=log(incomepc_t7_aux)
+
+
+*Child age at t=2
+gen agech_t2 = age_t0 + 2
+
+*Computing ATES for each year
+local i =1
+foreach x in 1 4 7{
+	bootstrap diff=r(ate), reps(`reps'): inc_diff incomepc_t`x'_aux
+	mat ate_inc[`i',1] = e(b)
+	mat se_ate_inc[`i',1] = e(se)
+	local i = `i' + 1
+
+}
+
+
+*Computing ATEs before
+egen child_id=group(sampleid child)
+mat ate_inc_2=J(1,1,.)
+mat se_ate_inc_2=J(1,1,.)
+
+*Before
+preserve
+keep incomepc_t1 d_RA child_id agech_t2
+
+bootstrap diff=r(ate), reps(`reps'): inc_diff incomepc_t1
+mat ate_inc_2[1,1]=e(b)
+mat se_ate_inc_2[1,1]=e(se)
+
+restore
+
+
+
+
+*For each year
+preserve
+clear
+set obs 3
+svmat ate_inc
+outsheet using "/home/jrodriguez/understanding_NH/results/Model/fit/ate_inc.csv", comma replace
+restore
+
+preserve
+clear
+set obs 3
+svmat se_ate_inc
+outsheet using "/home/jrodriguez/understanding_NH/results/Model/fit/se_ate_inc.csv", comma replace
+restore
+
+*Before
+preserve
+clear
+set obs 1
+svmat ate_inc_2
+outsheet using "/home/jrodriguez/understanding_NH/results/Model/fit/ate_inc_2.csv", comma replace
+restore
+
+preserve
+clear
+set obs 1
+svmat se_ate_inc_2
+outsheet using "/home/jrodriguez/understanding_NH/results/Model/fit/se_ate_inc_2.csv", comma replace
+restore
+
+
+exit, STATA clear
+
+
+
+
+
